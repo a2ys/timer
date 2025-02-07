49907e475aedfd5a872a58b4e77b672ad0d5f6a8
Aayush Shukla <oreus.aks@gmail.com>
2025-02-08 00:45:01 +0530

    fix: build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,11 +6,11 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen p-24 text-center">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <h2 className="text-3xl font-semibold mb-6">
-        Great Scott! We've hit a temporal anomaly!
+        Great Scott! We&rsquo;ve hit a temporal anomaly!
       </h2>
       <p className="text-xl mb-8">
-        It seems you've stumbled into a time vortex. This page doesn't exist...
-        yet.
+        It seems you&rsquo;ve stumbled into a time vortex. This page
+        doesn&rsquo;t exist... yet.
       </p>
       <Button asChild>
         <Link href="/">Travel back to the present</Link>
